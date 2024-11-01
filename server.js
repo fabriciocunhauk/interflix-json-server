1a8e942c883fc970ee1c46c706f392643590bda1
fabriciocunhauk <fabriciocunhadeveloper@gmail.com>
2024-11-01 16:14:15 +0000

    Updates

--- server.js
+++ server.js
@@ -6,6 +6,13 @@ const middlewares = jsonServer.defaults()
 const port = process.env.PORT || 8080;
 
 server.use(middlewares);
+
+server.use(
+    jsonServer.rewriter({
+    "/*": "/$1",
+    })
+);
+
 server.use(router);
 server.listen(port, () => {
     console.log(`JSON Server is running in ${port}`);
